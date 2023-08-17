f7db75d9d9f3dbb842957bceca2c65d361cd69b9
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-08-18 00:23:04 +0100

    updateTodo

--- main.go
+++ main.go
@@ -140,6 +140,43 @@ func deleteTodo(w http.ResponseWriter, r *http.Request){
 	})
 }
 
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimSpace(chi.URLParam(r, "id"))
+
+	if !bson.IsObjectIdHex(id){
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message":"The id is invalid",
+		})
+		return
+	}
+
+	var t todo
+	
+	if err:=json.NewDecoder(r.Body).Decode(&t); err != nil {
+		rnd.JSON(w, http.StatusProcessing, err)
+		return
+	}
+
+	if t.Title == ""{
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message":"The title field id required",
+		})
+		return
+	}
+
+	if err:= db.C(collectionName).
+	Update(
+		bson.M{"_id":bson.ObjectIdHex(id)},
+		bson.M{"title":t.Title, "completed":t.Completed},
+	); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message":"failed to update todo",
+			"error":err,
+		})
+		return
+	}
+}
+
 func main() {
 	stopChan := make(chan os.Signal)
 	signal.Notify(stopChan, os.Interrupt)
